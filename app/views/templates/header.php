50188e6318709db061505172c6b83cbcdd7c82ec
agus zakariya <aguszakariya@gmail.com>
2023-11-01 12:07:42 +0800

    update

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -57,6 +57,10 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
   <!-- Theme style -->
+  <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images//favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images//favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
   <link rel="stylesheet" href="css/myStyle.css">
   <style>
